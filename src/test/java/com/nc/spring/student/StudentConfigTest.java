382b9637c8d1e15035739e021783af0a4705639b
Nassim CHERFAOUI <99206668+NCherfaoui@users.noreply.github.com>
2024-07-04 15:15:59 +0200

    add test

---
+++ src/test/java/com/nc/spring/student/StudentConfigTest.java
@@ -0,0 +1,11 @@
+package com.nc.spring.student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentConfigTest {
+
+        @org.junit.jupiter.api.Test
+        void commandLineRunner() {
+        }
+
+}
